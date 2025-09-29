ad89ce476f8ec67bf04e340e1e195bda5e01ca45
lapointek <lapointek.dev@gmail.com>
2025-09-29 14:21:49 -0400

    feat: add movement of snake by key press

--- snake_game/snake.py
+++ snake_game/snake.py
@@ -1,5 +1,6 @@
 from turtle import Turtle
 
+t = Turtle()
 STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 
@@ -8,6 +9,7 @@ class Snake:
     def __init__(self):
         self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
 
     def move(self):
         for seg_num in range(len(self.segments) - 1, 0, -1):
@@ -23,3 +25,15 @@ class Snake:
             new_segment.penup()
             new_segment.goto(position)
             self.segments.append(new_segment)
+
+    def up(self):
+        self.head.setheading(90)
+
+    def down(self):
+        self.head.setheading(270)
+
+    def left(self):
+        self.head.setheading(180)
+
+    def right(self):
+        self.head.setheading(0)
