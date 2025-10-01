6398febb5a0ddac63bcc801d2420d58ade5eb720
lapointek <lapointek.dev@gmail.com>
2025-10-01 11:28:44 -0400

    feat: add scoreboard class

--- snake_game/scoreboard.py
+++ snake_game/scoreboard.py
@@ -1,5 +1,23 @@
 from turtle import Turtle
 
-t = Turtle()
+ALIGNMENT = "center"
+FONT = ("Arial", 14, "normal")
 
-t.write("Scoreboard")
+
+class Scoreboard(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.score = 0
+        self.color("White")
+        self.penup()
+        self.goto(0, 270)
+        self.hideturtle()
+        self.update_scoreboard()
+
+    def update_scoreboard(self):
+        self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
+
+    def increase_score(self):
+        self.score += 1
+        self.clear()
+        self.update_scoreboard()
