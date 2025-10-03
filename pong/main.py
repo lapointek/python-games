91eb98c860cf2cf89c853383b7ab5799138ff0b8
lapointek <lapointek.dev@gmail.com>
2025-10-03 13:47:43 -0400

    refactor(paddle): remove paddle animation at game start
    refactor(game-loop): add while loop and boolean game_is_on

--- pong/main.py
+++ pong/main.py
@@ -5,9 +5,16 @@ screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+screen.tracer(0)
 paddle = Paddle()
 
 screen.listen()
 screen.onkey(paddle.go_up, "Up")
 screen.onkey(paddle.go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+
 screen.exitonclick()
