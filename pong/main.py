25219c53acaf73d588a49858e721068aecd9954b
lapointek <lapointek.dev@gmail.com>
2025-10-03 12:30:54 -0400

    feat: add paddle class and methods to move paddle with key press

--- pong/main.py
+++ pong/main.py
@@ -1,9 +1,13 @@
 from turtle import Screen
-
+from paddle import Paddle
 
 screen = Screen()
 screen.setup(width=800, height=600)
 screen.bgcolor("black")
 screen.title("Pong")
+paddle = Paddle()
 
+screen.listen()
+screen.onkey(paddle.go_up, "Up")
+screen.onkey(paddle.go_down, "Down")
 screen.exitonclick()
