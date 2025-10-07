834b8c53ac478196236b2863505dac1e03afaeb6
lapointek <lapointek.dev@gmail.com>
2025-10-07 14:36:22 -0400

    feat: add player movement with onkey press

--- turtle_crossing/main.py
+++ turtle_crossing/main.py
@@ -4,9 +4,19 @@ from player import Player
 from car_manager import CarManager
 from scoreboard import Scoreboard
 
+
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.tracer(0)
+
+# create a turtle player that starts at the bottom of the screen
+# and listen for the "Up" keypress to move the turtle north.
+
+player = Player()
+
+screen.listen()
+
+screen.onkey(player.go_up, "Up")
 
 game_is_on = True
 while game_is_on:
