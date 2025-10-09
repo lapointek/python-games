9873b8ded556917a99a3657885e7736f454461dc
lapointek <lapointek.dev@gmail.com>
2025-10-09 13:40:41 -0400

    feat: add reset game method

--- snake_game/main.py
+++ snake_game/main.py
@@ -38,13 +38,19 @@ while game_is_on:
         or snake.head.ycor() > 280
         or snake.head.ycor() < -280
     ):
-        game_is_on = False
-        scoreboard.game_over()
+        scoreboard.reset()
+        snake.reset()
 
-    for segment in snake.segments[1:]:
-        if snake.head.distance(segment) < 10:
-            game_is_on = False
-            scoreboard.game_over()
+    for segment in snake.segments:
+        if segment == snake.head:
+            pass
+        elif snake.head.distance(segment) < 10:
+            scoreboard.reset()
+            snake.reset()
+
+    # for segment in snake.segments[1:]:
+    #    if snake.head.distance(segment) < 10:
+    #        scoreboard.reset()
 
 
 screen.exitonclick()
